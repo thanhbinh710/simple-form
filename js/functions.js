d8c5ffef72da57e2a5516305626b59be141116c9
Binh Nguyen <thanhbinh710@gmail.com>
2016-07-04 20:03:23 +0800

    - Add in animation for step 3 - Complete step 1 - Need some edit for the form input on step 2 - Left with PhoneGap

--- js/functions.js
+++ js/functions.js
@@ -1,30 +1,9 @@
 /* ==============================================
-	Preload
-=============================================== */
-$(window).load(function() { // makes sure the whole site is loaded
-	$('#status').fadeOut(); // will first fade out the loading animation
-	$('#preloader').delay(250).fadeOut('slow'); // will fade out the white DIV that covers the website.
-	$('body').delay(250).css({'overflow':'visible'});
-	$('#sub_content').addClass('animated zoomIn');
-	$(window).scroll();
-	$('.number').each(function () {
-    $(this).prop('Counter',0).animate({
-        Counter: $(this).text()
-    }, {
-        duration: 2000,
-        easing: 'swing',
-        step: function (now) {
-            $(this).text(Math.ceil(now));
-        }
-    });
-});
-})
-/* ==============================================
 	Sticky nav
 =============================================== */
 $(window).scroll(function(){
     'use strict';
-    if ($(this).scrollTop() > 1){  
+    if ($(this).scrollTop() > 1){
         $('header').addClass("sticky");
     }
     else{
@@ -34,7 +13,6 @@ $(window).scroll(function(){
 /* ==============================================
 	Common
 =============================================== */
-new WOW().init();
 
 jQuery(function($) {
 	"use strict";
@@ -56,57 +34,8 @@ jQuery(function($) {
 	});
 });
 
-<!-- Magnific popup -->	
-$(function () {
-'use strict';
-$('.video_pop').magnificPopup({type:'iframe'});	/* video modal*/
-/* Gallery images modal*/
-$('.magnific-gallery').each(function() {
-    $(this).magnificPopup({
-        delegate: 'a', 
-        type: 'image',
-        gallery:{enabled:true},
-		removalDelay: 500, //delay removal by X to allow out-animation
-  callbacks: {
-    beforeOpen: function() {
-      // just a hack that adds mfp-anim class to markup 
-       this.st.image.markup = this.st.image.markup.replace('mfp-figure', 'mfp-figure mfp-with-anim');
-       this.st.mainClass = this.st.el.attr('data-effect');
-    }
-  },
-  closeOnContentClick: true,
-  midClick: true // allow opening popup on middle mouse click. Always set it to true if you don't provide alternative source.
-    });
-}); 
-}); 
 
-<!-- Radio and checkbox styles -->	
-$('input.icheck').iCheck({
-   checkboxClass: 'icheckbox_square-grey',
-   radioClass: 'iradio_square-grey'
- });
- 
-<!-- Collapse filters close on mobile-->	
-$(window).bind('resize load',function(){
-if( $(this).width() < 991 )
-{
-$('.collapse#collapseFilters').removeClass('in');
-$('.collapse#collapseFilters').addClass('out');
-}
-else
-{
-$('.collapse#collapseFilters').removeClass('out');
-$('.collapse#collapseFilters').addClass('in');
-}   
-});
-
-<!-- Tooltip -->	
-$('.tooltip-1').tooltip({html:true});
-
-<!-- Pace holder -->	
-$('input, textarea').placeholder();
-
-<!-- Accordion -->	
+<!-- Accordion -->
 function toggleChevron(e) {
     $(e.target)
         .prev('.panel-heading')
@@ -115,7 +44,7 @@ function toggleChevron(e) {
 }
 $('.panel-group').on('hidden.bs.collapse shown.bs.collapse', toggleChevron);
 
-<!-- Cat nav onclick active -->	
+<!-- Cat nav onclick active -->
 $('ul#cat_nav li a').on('click', function(){
     $('ul#cat_nav li a.active').removeClass('active');
     $(this).addClass('active');
